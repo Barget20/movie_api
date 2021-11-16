25c408de892ef004d3e23abc7ad7c6bbd3e85caf
Brian A <89669426+Barget20@users.noreply.github.com>
2021-11-16 16:34:11 -0500

    Homework 2.4 additions
    1) added let topMovie, as well as titles and directors
    2) added const morgan, const bodyParser
    3) added app.get and app.use features

--- index.js
+++ index.js
@@ -1,2 +1,86 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
+const bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
+
+let topMovies = [
+    {
+        title: 'Ready Player One',
+        director: 'Steven Spielberg'
+    },
+    {
+        title: 'Deadpool',
+        director: 'Tim Miller'
+    }, 
+    {
+        title: 'Fight Club',
+        director: 'David Fincher'
+    },
+    {
+        title: 'Inception',
+        director: 'Christopher Nolan'
+    },
+    {
+        title: 'Moneyball',
+        director: 'Bennett Miller'
+    },
+    {
+        title: 'The Nightmare Before Christmas',
+        director: 'Henry Selick'
+    },
+    {
+        title: 'Pirates of the Caribbean: The Curse of the Black Pearl',
+        director: 'Gore Verbinski'
+    },
+    {
+        title: 'Paper Towns',
+        director: 'Jack Schreier'
+    },
+    {
+        title: 'Sherlock Holmes (2009)',
+        director: 'Guy Ritchie'
+    },
+    {
+        title: 'That Thing You Do',
+        director: 'Tom Hanks'
+    },
+
+]
+
+app.use(morgan('common'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// GET requests
+app.get('/', (req, res)=> {
+    res.send('Welcome to my movie club!');
+});
+
+app.get('/unknown', (req, rest) => {
+    res.send('We are currently under construction.');
+});
+
+app.get('/documentation', (req, res) => {
+    res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+
+
+//listen for requests
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
