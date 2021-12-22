7987b5c312e92dd362178580ac3761db0d1b712a
Brian A <89669426+Barget20@users.noreply.github.com>
2021-12-22 16:56:00 -0500

    Minor Fixes
    1) changed Username to accountInfo as previously it was used.
    2) changed app.delete for removing a favorited movie from the list.

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ app.post('/users', (req, res) => {
 
 //changed back to accountInfo instead of username//
 app.put('/users/:accountInfo', (req, res) => {
-    Users.findOneAndUpdate({ Username: req.params.Username}, { $set: {
+    Users.findOneAndUpdate({ Username: req.params.accountInfo}, { $set: {
         Username: req.body.Username,
         Password: req.body.Password,
         Email: req.body.Email,
@@ -230,8 +230,8 @@ app.put('/users/:accountInfo', (req, res) => {
     //res.send('allows user access to their account information to edit/update user name'));
 
 app.post('/users/:accountInfo/favoritesList/:movieID', (req, res) => {
-    Users.findOneAndUpdate({ Username: req.params.Username}, {
-        $push: {FavoriteMovies: req.params.MovieID}
+    Users.findOneAndUpdate({ Username: req.params.accountInfo}, {
+        $push: {FavoriteMovies: req.params.movieID}
     },
     { new:true}, 
     (err, updatedUser) => {
@@ -247,27 +247,28 @@ app.post('/users/:accountInfo/favoritesList/:movieID', (req, res) => {
 //Delete Requests
 
 app.delete('/users/:accountInfo/favoritesList/:movieID', (req, res) => {
-    Movies.findOneAndUpdate({ MovieID: req.params.MovieID})
-    .then((movie) => {
-        if (!movie) {
-            res.status(400).send(req.params.MovieID + ' was not found');
+    Users.findOneAndUpdate({ Username: req.params.accountInfo}, {
+        $pull: {
+            FavoriteMovies: req.params.movieID}
+    },
+    {new:true}, 
+    (err, movieRemoved) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
         } else {
-            res.status(200).send(req.params.MovieID + ' was deleted.');
+            res.json(movieRemoved);
         }
-    })
-    .catch((err) => {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
     });
 });
 
 app.delete('/users/:accountInfo', (req, res) => {
-    Users.findOneAndRemove({ Username: req.params.Username})
+    Users.findOneAndRemove({ Username: req.params.accountInfo})
     .then((user) => {
         if (!user) {
-            res.status(400).send(req.params.Username + ' was not found');
+            res.status(400).send(req.params.accountInfo + ' was not found');
         } else {
-            res.status(200).send(req.params.Username + ' was deleted.');
+            res.status(200).send(req.params.accountInfo + ' was deleted.');
         }
     })
     .catch((err) => {
