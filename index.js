91f8ebd27de9d430aa2be3ea9fa5d0df90095b72
Brian A <89669426+Barget20@users.noreply.github.com>
2022-01-19 16:31:18 -0500

    opened up cors temporarily for assignment

--- index.js
+++ index.js
@@ -24,18 +24,20 @@ app.use(bodyParser.urlencoded({
 }));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', '*'];
+//let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', '*'];
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            let message = 'The CORS policy for this application doesnt allow access from origin' + origin;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
-    }
-}));
+app.use(cors());
+
+//app.use(cors({
+    //origin: (origin, callback) => {
+        //if(!origin) return callback(null, true);
+        //if(allowedOrigins.indexOf(origin) === -1){
+            //let message = 'The CORS policy for this application doesnt allow access from origin' + origin;
+            //return callback(new Error(message), false);
+        //}
+        //return callback(null, true);
+    //}
+//}));
 
 
 let auth = require('./auth')(app);
@@ -56,7 +58,8 @@ app.get('/', (req, res) => {
 })
 
   //Returns full list of movies
-app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
+app.get('/movies', // passport.authenticate('jwt', {session: false}), 
+    (req, res) => {
     Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
