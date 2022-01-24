f93286255aec5944871bf9fe5342c4fa6cdea37e
Brian A <89669426+Barget20@users.noreply.github.com>
2022-01-24 16:46:18 -0500

    Reactivated jwt
    1) reactivated the passport.authenticate jwt under the app.get/movies

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.get('/', (req, res) => {
 })
 
   //Returns full list of movies
-app.get('/movies', // passport.authenticate('jwt', {session: false}), 
+app.get('/movies', passport.authenticate('jwt', {session: false}), 
     (req, res) => {
     Movies.find()
         .then((movies) => {
